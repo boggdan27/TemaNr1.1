033c45eaaa2d24941cbf33ffb997a0164cfcd00d
BogDa <justbogda@gmail.com>
2020-12-05 13:08:43 +0200

    Issue bogda - rebase
    implementare CMyThread, rezolvare conflict
    Signed-off-by: bogda <justbogda@gmail.com>
    Signed-off-by: BogDa <justbogda@gmail.com>

--- src/mta/Tema1/CMyThread.java
+++ src/mta/Tema1/CMyThread.java
@@ -1,4 +1,19 @@
 package mta.Tema1;
 
 public class CMyThread {
+    int thread_id;
+
+    public CMyThread(int thread_id) {
+        this.thread_id = thread_id;
+    }
+
+    public void run()
+    {
+		//am adaugat o linie
+		
+    }
+
+    public int getThread_id() {
+        return thread_id;
+    }
 }

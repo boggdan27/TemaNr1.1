19cbc9e220475372c3ef8dcfefbb8637434afe51
Oana Novac <novac.oana@yahoo.com>
2020-12-10 12:33:45 +0200

    Implementare functia download_page si extract_links
    Issue:#12
    Am implementat functiile download_page si extract_links
    din clasa CCrawlerHandler si am adaugat un membru pentru
    calea url-ului curent
    
    Signed-off-by: Oana Novac <novac.oana@yahoo.com>

--- src/mta/Tema1/Main.java
+++ src/mta/Tema1/Main.java
@@ -1,8 +1,14 @@
 package mta.Tema1;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+
         ICrawler c = CCrawler.getInstance();
+
     }
+
+
+
 }
